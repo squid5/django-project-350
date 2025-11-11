b8ab6704b3be779b307ff84b740774c309bc5e8c
Kaitlyn V <kvenzen99@gmail.com>
2025-11-11 12:52:04 -0500

    Re-edited the files to say 11_project.

--- 11_project/urls.py
+++ 11_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for learning_log project.
+URL configuration for 11_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
